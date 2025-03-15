3518f7ad9982bd4c9d2bfedaa3ed59fe3f753357
yukaifan <1092882580@qq.com>
2025-03-15 18:32:11 +0800

    Initial commit

---
+++ src/main/java/com/code4j/ai/mcp/McpAllApplication.java
@@ -0,0 +1,13 @@
+package com.code4j.ai.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code4j/ai/mcp/McpAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4j.ai.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
